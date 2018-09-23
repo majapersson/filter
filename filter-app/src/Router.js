8815d49b9d39e0eab0245bc27d9abee1ab01e150
Maja Persson <persson.maja@gmail.com>
2018-09-23 18:48:58 +0200

    Global styles and fonts

--- filter-app/src/Router.js
+++ filter-app/src/Router.js
@@ -1,14 +1,31 @@
-import React from "react";
+import React, { Component } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { injectGlobal } from "styled-components";
 
 import Article from "./pages/Article";
 
-const Router = () => (
-  <BrowserRouter>
-    <Switch>
-      <Route path="/:id" render={props => <Article {...props} />} />
-    </Switch>
-  </BrowserRouter>
-);
+const GlobalStyles = injectGlobal`
+  * {
+    box-sizing: border-box;
+  }
+
+  body {
+    color: #333;
+    font-family: 'Merriweather', serif;
+    line-height: 1.75;
+  }
+`;
+
+class Router extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route path="/:id" render={props => <Article {...props} />} />
+        </Switch>
+      </BrowserRouter>
+    );
+  }
+}
 
 export default Router;
