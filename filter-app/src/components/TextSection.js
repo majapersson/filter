fb87c3062070272f3cabb0c0feec7a0e6dda925e
Maja Persson <persson.maja@gmail.com>
2018-09-23 18:50:24 +0200

    Styling of text section

--- filter-app/src/components/TextSection.js
+++ filter-app/src/components/TextSection.js
@@ -2,13 +2,35 @@ import React, { Component } from "react";
 import ReactHtmlParser from "react-html-parser";
 import styled from "styled-components";
 
+const RightText = styled.section`
+  display: flex;
+  justify-content: center;
+  padding: 12.5vh 0;
+  width: 100%;
+`;
+
+const Content = styled.div`
+  margin-right: 12.5vw;
+  max-width: 37.5rem;
+`;
+
+const Lead = styled.p`
+  font-size: 1.5rem;
+  font-style: italic;
+  line-height: 2.5rem;
+  margin-bottom: 3rem;
+`;
+
 class TextSection extends Component {
   render() {
     return (
-      <section>
-        <p className="lead-paragraph">{this.props.lead}</p>
-        {ReactHtmlParser(this.props.content)}
-      </section>
+      <RightText className="TextSection">
+        <Content>
+          {this.props.lead ? <Lead>{this.props.lead}</Lead> : ""}
+
+          {ReactHtmlParser(this.props.content)}
+        </Content>
+      </RightText>
     );
   }
 }
