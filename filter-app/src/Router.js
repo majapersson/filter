41a05a163bcd02459f937aaf727fae717747b93b
Maja Persson <persson.maja@gmail.com>
2018-10-01 23:28:10 +0200

    Moved global styles to css-file

--- filter-app/src/Router.js
+++ filter-app/src/Router.js
@@ -1,54 +1,10 @@
 import React, { Component } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import { injectGlobal, ThemeProvider } from "styled-components";
+import { ThemeProvider } from "styled-components";
 
+import Navigation from "./components/navigation/Navbar/Navigation";
 import Article from "./pages/Article";
 import Home from "./pages/Home";
-
-injectGlobal`
-  @font-face {
-    font-family: "National";
-    src: url("/assets/fonts/National-Regular.otf") format("opentype");
-    font-weight: 400;
-  }
-
-  @font-face {
-    font-family: "National";
-    src: url("/assets/fonts/National-Light.otf") format("opentype");
-    font-weight: 100;
-  }
-
-  @font-face {
-    font-family: "National";
-    src: url("/assets/fonts/National-Bold.otf") format("opentype");
-    font-weight: 700;
-  }
-
-  @font-face {
-    font-family: "National LI";
-    src: url("/assets/fonts/National-ThinItalic.otf") format("opentype");
-  }
-
-  @font-face {
-    font-family: "Tiempos Headline";
-    src: url("/assets/fonts/TiemposHeadline-Black.otf") format("opentype");
-  }
-
-  @font-face {
-    font-family: "Tiempos Regular";
-    src: url("/assets/fonts/TiemposText-Regular.otf") format("opentype");
-  }
-
-  * {
-    box-sizing: border-box;
-  }
-
-  body {
-    color: #333;
-    font-family: "National", sans-serif;
-    line-height: 1.75;
-  }
-`;
 
 class Router extends Component {
   render() {
